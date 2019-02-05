ad3f37e4ec49a615088e31f5ed01ba341c7ff57b
David Quast <dquast@ryerson.ca>
2019-02-05 11:01:27 -0500

    Change package name before release to NPM.

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'msc',
+  namespace: 'rl-msc',
   plugins: [
     sass({
       includePaths: ['./node_modules', './src/global']
